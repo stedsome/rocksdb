381f9c2ebee881b3ccfb59ff8c70298b0e821197
yyf <stedyyf@gmail.com>
2020-04-27 07:50:59 -0700

    minor

--- env/io_posix.cc
+++ env/io_posix.cc
@@ -1076,7 +1076,7 @@ IOStatus PosixWritableFile::AsyncAppend(const Slice& data, const IOOptions& /*op
   // const char* src = data.data();
   size_t nbytes = data.size();
   struct io_uring* iu = nullptr;
-  int ret = io_uring_queue_init(kIoUringDepth, iu, 0);
+  io_uring_queue_init(kIoUringDepth, iu, 0);
 
   /*IOStatus s = WaitQueue(100);
 
@@ -1238,7 +1238,7 @@ IOStatus PosixWritableFile::AsyncSync(const IOOptions& /*opts*/,
     return s;
   }*/
   struct io_uring* iu = nullptr;
-  int ret = io_uring_queue_init(kIoUringDepth, iu, 0);
+  io_uring_queue_init(kIoUringDepth, iu, 0);
 
   struct io_uring_sqe* sqe = io_uring_get_sqe(iu);
   if (sqe == nullptr) {
@@ -1274,7 +1274,7 @@ IOStatus PosixWritableFile::AsyncRangeSync(uint64_t offset, uint64_t nbytes) {
     return s;
   }*/
   struct io_uring* iu = nullptr;
-  int ret = io_uring_queue_init(kIoUringDepth, iu, 0);
+  io_uring_queue_init(kIoUringDepth, iu, 0);
 
 
   struct io_uring_sqe* sqe = io_uring_get_sqe(iu);
