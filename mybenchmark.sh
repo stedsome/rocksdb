7257e40cab114cc9768cde9d9769be5637bf246a
yyf <stedyyf@gmail.com>
2020-05-19 17:08:49 -0600

    clean

--- mybenchmark.sh
+++ mybenchmark.sh
@@ -2,4 +2,5 @@ export DB_DIR=/home/reshma/rocksdb
 export WAL_DIR=/home/reshma/wal
 export TEMP=/home/reshma/tmp
 export OUTPUT_DIR=/home/reshma/ouput
-tools/benchmark.sh fillseq_disable_wal
+#tools/benchmark.sh fillseq_disable_wal
+NUM_KEYS=100000000 NUM_THREADS=1 tools/benchmark.sh readwhilewriting
