21a942c2b8cd4607ce4bf9f3b5f42e4105bd8852
yyf <stedyyf@gmail.com>
2020-05-19 17:52:24 -0700

    a

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -2025,10 +2025,9 @@ class Stats {
         } else {
 
            fprintf(stderr,
-                  "%s ... thread %d: (%" PRIu64 ",%" PRIu64 ") ops and "
-                  "(%.1f,%.1f) ops/second, (%.1f,%.1f) read ops/second, (%.1f,%.1f) read ops/second, in (%.6f,%.6f) seconds\n",
+                  "%s ... thread %d: (%" PRIu64 ",%" PRIu64 ") ops and ""(%.1f,%.1f) ops/secondtyreer, (%.1f,%.1f) read ops/second, (%.1f,%.1f) read ops/second, in (%.6f,%.6f) seconds\n",
                   FLAGS_env->TimeToString(now/1000000).c_str(),
-                  id_i,
+                  id_,
                   done_ - last_report_done_, done_,
                   (done_ - last_report_done_) /
                   (usecs_since_last / 1000000.0), 
